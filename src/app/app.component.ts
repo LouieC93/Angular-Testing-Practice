d4dfb56a7a94e1c48c7474aaeed86404170d2604
Louie Chen <louieforcode@gmail.com>
2022-01-25 15:28:49 +0800

    [Day - 02]--Finish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,52 @@
 import { Component } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'form-and-testing';
+  account = '123';
+  pwd = 'abc';
+  accountErrorMsg = '';
+  pwdErrorMsg = '';
+
+  accountValueChange(acc: string, error: ValidationErrors | null) {
+    this.account = acc;
+    this.runValid(error, 'account');
+  }
+
+  pwdValueChange(pwd: string, error: ValidationErrors | null) {
+    this.pwd = pwd;
+    this.runValid(error, 'pwd');
+  }
+
+  private runValid(error: ValidationErrors | null, controlName: 'account' | 'pwd') {
+    let msg = '';
+    if (!error) {
+      msg = '';
+    } else if (error['required']) {
+      msg = 'this is required!';
+    } else if (error['pattern']) {
+      msg = 'pattern error!';
+    } else if (error['minlength']) {
+      msg = 'minlength 8!';
+    } else {
+      msg = 'unknown error!';
+    }
+    this.setErrorMsg(controlName, msg);
+  }
+
+  private setErrorMsg(controlName: 'account' | 'pwd', errorMsg: string) {
+    if (controlName === 'account') {
+      this.accountErrorMsg = errorMsg;
+    } else {
+      this.pwdErrorMsg = errorMsg;
+    }
+  }
+
+  log(...param: any[]) {
+    console.log(...param);
+  }
 }
