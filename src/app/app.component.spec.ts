92f94e106e4f045c758fd76d5c1d344f5b586e80
Louie Chen <louieforcode@gmail.com>
2022-01-27 15:02:07 +0800

    [Day-06]--Finish

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,56 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
+  let fixture: ComponentFixture<AppComponent>;
+  let app: AppComponent;
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule, FormsModule],
+      declarations: [AppComponent],
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    app = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'form-and-testing'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('form-and-testing');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('form-and-testing app is running!');
+  describe('function accountValueChange', () => {
+    it('should set value into property "account"', () => {
+      const acc = 'aa@aa.com';
+      const err = null;
+      app.accountValueChange(acc, err);
+      expect(app.account).toBe(acc);
+    });
+    it('should set error msg "this is required!" into property "accountError" when enter empty string', () => {
+      const acc = '';
+      const error = { required: true };
+      const msg = 'this is required!';
+      app.accountValueChange(acc, error);
+      expect(app.accountErrorMsg).toBe(msg);
+    });
+    it('should set error msg "pattern error!" into property "accountError" when enter invalid string', () => {
+      const acc = 'aaaa1111';
+      const error = {
+        pattern: {
+          actualValue: acc,
+          requiredpattern: '^\\b[\\w\\.-]+@[\\w\\.-]+\\.\\w{2,4}\\b$',
+        },
+      };
+      const msg = 'pattern error!';
+      app.accountValueChange(acc, error);
+      expect(app.accountErrorMsg).toBe(msg);
+    });
+    it('should set error msg "" into property "accountError" when enter correct string', () => {
+      const acc = 'aaaa1111@bbb.com';
+      const error = null;
+      const msg = '';
+      app.accountValueChange(acc, error);
+      expect(app.accountErrorMsg).toBe(msg);
+    });
   });
 });
